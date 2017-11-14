d562a0a4c16882ace89b65e38b1dc96b17f54c8f
MaxiProger <kolotseymax@gmail.com>
2017-11-14 12:13:14 +0300

    new

---
+++ app/src/main/java/com/example/kolot/http_trying/Application.java
@@ -0,0 +1,15 @@
+package com.example.kolot.http_trying;
+
+import ly.img.android.PESDK;
+
+/**
+ * Created by max on 11/14/17.
+ */
+
+public class Application extends android.app.Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        PESDK.init(this, "android_license.dms");
+    }
+}
